20d363db8f50e3b6f2d754b3d3a3bde23eccf226
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-07 20:52:24 -0300

    Martelo101 - 28 - Adding 2D User Interface Text

---
+++ Martelo101/Assets/TextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("space"))
+        {
+            text.text = "Space key pressed!";
+        }
+	}
+}
