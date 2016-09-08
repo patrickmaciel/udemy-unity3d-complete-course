d49bac2243b0fe6cdf67cb2cff1f151e23f5d095
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-07 21:16:00 -0300

    Martelo101 - 28 - Word Wrapping & Joining Strings

--- Martelo101/Assets/TextController.cs
+++ Martelo101/Assets/TextController.cs
@@ -8,13 +8,17 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	}
+        text.text = "Marllon, um desenvolvedor de jogos, durante o desenvolvimento do game \"Tabuada na Fazenda\", " +
+                "se deparou com um bug crítico que prejudicava os dados da aplicação.\n\n" +
+                "No mesmo momento, seu gerente Joás, que está na sala ao lado, enviou uma mensagem no Skype dizendo: \"estou indo ai pra gente conversar\".\n\n" +
+                "O que Marllon deve fazer?";
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown("space"))
         {
-            text.text = "Space key pressed!";
+            text.text = "Vai saber...";
         }
 	}
 }
