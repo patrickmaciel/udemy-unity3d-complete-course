6c835771466207d5a7fd377e3760914f11e064cd
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-11 17:01:54 -0300

    47. Calling scripts from buttons

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("See you soon!");
+    }
+}
