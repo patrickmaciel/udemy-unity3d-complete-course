19e12eaa24a2459e59417f590ae61fb2b680665f
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-21 22:15:17 -0300

    62. import the menu system

---
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+        // SceneManager.LoadScene();
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("See you soon!");
+        Application.Quit();
+    }
+
+    public void ReturnToStart()
+    {
+        Application.LoadLevel("Start");
+        // SceneManager.LoadScene
+    }
+}
