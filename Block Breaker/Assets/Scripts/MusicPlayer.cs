a6bd2a6d6de36312dc9107c9ace8e406090fa7a2
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-25 14:15:37 -0300

    65. using static variables

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
 
 	// Use this for initialization
 	void Start () {
-        GameObject.DontDestroyOnLoad(gameObject);	
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
