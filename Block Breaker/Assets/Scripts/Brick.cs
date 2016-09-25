9dbd5e153bbf9ffb3182c8dedfce9c9a7633557a
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-25 20:18:28 -0300

    77. reating & using unity prefabs

---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHits;
+    public int timesHit;
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+        maxHits = 1; // Random Range 1-3
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
