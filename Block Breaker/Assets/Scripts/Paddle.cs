397804d9e7784e9a72e96774612f04c54f1dae55
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-25 17:52:47 -0300

    74. movement by mouse

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        Vector3 paddlePosition = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(
+            // mousePosInBlocks,
+            Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f),
+            paddlePosition.y,
+            paddlePosition.z
+        );
+	
+	}
+}
