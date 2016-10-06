9684ee202a302561cab296b1c0bdd964ee9f95f2
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-05 21:44:41 -0300

    88. using random.range()

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -30,7 +30,11 @@ public class Ball : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
         if (hasStarted)
+        {
             this.GetComponent<AudioSource>().Play();
+            this.GetComponent<Rigidbody2D>().velocity += tweak;
+        }
     }
 }
