0222f4d8c71d26e48440bbcced6a3103b6a2203b
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-09 20:01:55 -0300

    104. restrinct the player position

--- LaserDefender/Assets/Scripts/PlayerController.cs
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Security.Cryptography;
 
 public class PlayerController : MonoBehaviour
 {
     private float speed = 15.0f;
+    private float padding = 0.5f;
+    private float xmin, xmax;
 
     // Use this for initialization
     void Start()
     {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
 
     }
 
@@ -16,12 +24,18 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.left*speed*Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.right* speed * Time.deltaTime;
         }
+
+        // restrict player to the gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 
         //Vector3 moveDirection = gameObject.transform.position - _origPos;
         //if (moveDirection != Vector3.zero)
