6aa4b8d5acc30c6005970570bf73c717c15a842c
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-09 14:06:07 -0300

    89. automated play testing (tryting to fix the shit paddle)

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
+    public bool autoPlay = false;
+    private Ball ball;
 
     // Use this for initialization
-    void Start () {
-    
+    void Start ()
+    {
+        ball = Object.FindObjectOfType<Ball>();
     }
     
     // Update is called once per frame
     void Update () {
+        if (!autoPlay)
+        {
+            MoveWithMouse();
+        }
+        else
+        {
+            AutoPlay();   
+        }     
+    }
+
+    void AutoPlay()
+    {
+        float ballPosInBlocks = ball.transform.position.x;
+        Vector3 paddlePosition = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(
+            Mathf.Clamp(ballPosInBlocks, 0f, 15f),
+            paddlePosition.y,
+            paddlePosition.z
+        );
+    }
+
+    void MoveWithMouse()
+    {
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
         Vector3 paddlePosition = gameObject.transform.position;
         gameObject.transform.position = new Vector3(
@@ -18,6 +45,5 @@ public class Paddle : MonoBehaviour {
             paddlePosition.y,
             paddlePosition.z
         );
-    
     }
 }
