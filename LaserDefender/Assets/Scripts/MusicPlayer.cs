c0d1eeb3ad84112a33026808269cf15ba1c5bb33
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-09 18:40:11 -0300

    102. importing the menu system

---
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        Debug.Log("MusicPlayer Awake - " + GetInstanceID());
+
+        if (instance != null)
+        {
+            Debug.Log("Instancia atual != null - " + instance.GetInstanceID());
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            Debug.Log("Instancia atual null - " + instance.GetInstanceID());
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("MusicPlayer Start - " + GetInstanceID());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
