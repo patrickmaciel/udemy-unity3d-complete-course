673876bd811ec78a227dd3f59614178278b9402b
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-16 19:34:54 -0200

    113. detecting enemies have been destroyed

--- LaserDefender/Assets/Scripts/EnemySpawnController.cs
+++ LaserDefender/Assets/Scripts/EnemySpawnController.cs
@@ -14,18 +14,22 @@ public class EnemySpawnController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    float distance = transform.position.z - Camera.main.transform.position.z;
+		float distance = transform.position.z - Camera.main.transform.position.z;
 	    Vector3 leftBoundry = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
 	    Vector3 rightBoundry = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
 	    xmin = leftBoundry.x;
 	    xmax = rightBoundry.x;
 
+	    SpawnEnemies();
+	}
+
+	void SpawnEnemies()
+	{
 	    foreach (Transform child in transform)
 	    {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child;
         }
-	    
 	}
 	
 	// Update is called once per frame
@@ -46,6 +50,24 @@ public class EnemySpawnController : MonoBehaviour
 		} else if (rightEdgeOfFormation > xmax) {
             movingRight = false;
         }
+
+		if (AreAllEnemiesDead())
+		{
+			SpawnEnemies();
+		}
+	}
+
+	bool AreAllEnemiesDead()
+	{
+		foreach (Transform childCount in transform)
+		{
+			if (childCount.childCount > 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 
     void OnDrawGizmos()
