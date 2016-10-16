b2a92acfade1effc423da9461d1c2f0f35cd1e6f
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-16 13:49:58 -0200

    Added VS Code plugin

--- LaserDefender/Assets/Scripts/EnemySpawnController.cs
+++ LaserDefender/Assets/Scripts/EnemySpawnController.cs
@@ -4,10 +4,22 @@ using System.Collections;
 public class EnemySpawnController : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 8f;
+
+    private float speed = 5f;
+    private float xmin, xmax;
+    private bool movingRight;
 
 	// Use this for initialization
 	void Start ()
 	{
+	    float distance = transform.position.z - Camera.main.transform.position.z;
+	    Vector3 leftBoundry = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+	    Vector3 rightBoundry = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+	    xmin = leftBoundry.x;
+	    xmax = rightBoundry.x;
+
 	    foreach (Transform child in transform)
 	    {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
@@ -18,6 +30,26 @@ public class EnemySpawnController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (movingRight)
+	    {
+	        transform.position += Vector3.right * speed * Time.deltaTime;
+	    }
+	    else
+	    {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+	    float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+	    float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+	    if (leftEdgeOfFormation < xmin) {
+			movingRight = true;
+		} else if (rightEdgeOfFormation > xmax) {
+            movingRight = false;
+        }
 	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
 }
