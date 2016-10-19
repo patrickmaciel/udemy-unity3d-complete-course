3eab235b236aa5e2c9a19bff4bee130dc4611697
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-19 20:18:07 -0200

    121. polishing the menu system II

--- LaserDefender/Assets/Scripts/MusicPlayer.cs
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
     static MusicPlayer instance = null;
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+    private static AudioSource music;
 
     void Awake()
     {
         Debug.Log("MusicPlayer Awake - " + GetInstanceID());
+    }
 
-        if (instance != null)
+	// Use this for initialization
+	void Start () {
+        Debug.Log("MusicPlayer Start - " + GetInstanceID());
+
+        if (instance != null && instance != this)
         {
             Debug.Log("Instancia atual != null - " + instance.GetInstanceID());
             Destroy(gameObject);
@@ -18,16 +27,37 @@ public class MusicPlayer : MonoBehaviour {
             instance = this;
             Debug.Log("Instancia atual null - " + instance.GetInstanceID());
             GameObject.DontDestroyOnLoad(gameObject);
-        }
-    }
-
-	// Use this for initialization
-	void Start () {
-        Debug.Log("MusicPlayer Start - " + GetInstanceID());
+            music = GetComponent<AudioSource>();
+            OnLevelWasLoaded(0);
+        }        
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (music != null && music.clip != null)
+        {
+            music.Stop();
+        }
+
+        if (level == 0)
+        {
+            music.clip = startClip;
+        }
+        else if (level == 1)
+        {
+            music.clip = gameClip;
+        }
+        else if (level == 2)
+        {
+            music.clip = endClip;
+        }
+
+        music.loop = true;
+        music.Play();
+    }
 }
